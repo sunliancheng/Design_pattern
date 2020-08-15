b93e8af04c59045e1480de25229f67c94a9f3d85
wangsimin <taoyiran815579058@outlook.com>
2020-08-15 09:40:21 +0800

    dp

--- src/com/xxx/designPattern/Decorator_Pattern/DecoratorPatternDemo.java
+++ src/com/xxx/designPattern/Decorator_Pattern/DecoratorPatternDemo.java
@@ -1,4 +1,16 @@
 package com.xxx.designPattern.Decorator_Pattern;
 
 public class DecoratorPatternDemo {
+
+    public static void main(String[] args) {
+        Shape circle = new Circle();
+        ShapeDecorator redCircle = new RedShapeDecrator(new Circle());
+        ShapeDecorator redRectangle = new RedShapeDecrator(new Rectangle());
+
+        circle.draw();
+
+        redCircle.draw();
+
+        redCircle.draw();
+    }
 }
